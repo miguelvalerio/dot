2cedf643fef4bcaaf0a952d230f3126ab6a81fd7
miguelvalerio <miguelgomes.valerio@gmail.com>
2022-05-21 17:52:57 +0000

    fix polybar, added dependencies

--- setup.sh
+++ setup.sh
@@ -2,4 +2,18 @@
 
 set -eo pipefail
 
-yay -S i3-gaps betterlockscreen gvim powerline-fonts urxvt rofi spotify
+sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
+ 
+yay -S i3-gaps betterlockscreen gvim powerline-fonts rofi spotify rxvt-unicode stow polybar nerd-fonts-complete
+
+yay -S zsh zsh-completions
+
+cd .zprezto && git submodule sync && git submodule update --init
+
+chsh -s /usr/bin/zsh
+
+stow -R -v .
+
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+vim +PluginInstall +qall
